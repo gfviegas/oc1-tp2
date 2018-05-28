e356c7a89af6c3b36ff4f9054c21314ec1c1a7c8
Gustavo Viegas <webviegas@gmail.com>
2018-05-28 15:01:20 -0300

    Modulo Registrador

--- registers.v
+++ registers.v
@@ -8,4 +8,48 @@ module registers (
   output wire [31:0] readData2 // Dado armazenado no registrador register2
 );
 
+  reg [31:0] registradores [31:0];
+
+  initial begin
+    registradores[0] <= 0;
+    registradores[1] <= 0;
+    registradores[2] <= 0;
+    registradores[3] <= 0;
+    registradores[4] <= 0;
+    registradores[5] <= 0;
+    registradores[6] <= 0;
+    registradores[7] <= 0;
+    registradores[8] <= 0;
+    registradores[9] <= 0;
+    registradores[10] <= 0;
+    registradores[11] <= 0;
+    registradores[12] <= 0;
+    registradores[13] <= 0;
+    registradores[14] <= 0;
+    registradores[15] <= 0;
+    registradores[16] <= 0;
+    registradores[17] <= 0;
+    registradores[18] <= 0;
+    registradores[19] <= 0;
+    registradores[20] <= 0;
+    registradores[21] <= 0;
+    registradores[22] <= 0;
+    registradores[23] <= 0;
+    registradores[24] <= 0;
+    registradores[25] <= 0;
+    registradores[26] <= 0;
+    registradores[27] <= 0;
+    registradores[28] <= 0;
+    registradores[29] <= 0;
+    registradores[30] <= 0;
+    registradores[31] <= 0;
+  end
+
+  always @(posedge regWrite) begin
+    registradores[writeRegister] = writeData;
+  end
+
+  assign readData1 = registradores[register1];
+  assign readData2 = registradores[register2];
+
 endmodule

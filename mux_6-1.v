860381801328919f6e21450962267b056641b9b0
Gustavo Viegas <webviegas@gmail.com>
2018-05-28 12:31:32 -0300

    Ajustando mux da ULA

---
+++ mux_6-1.v
@@ -0,0 +1,24 @@
+// Multiplexador de 6 entradas simples.
+module mux6_1 (
+  input wire entrada1,
+  input wire entrada2,
+  input wire entrada3,
+  input wire entrada4,
+  input wire entrada5,
+  input wire entrada6,
+  input wire [3:0] seletor,
+  output reg saida
+);
+
+  always @ (entrada1 or entrada2 or entrada3 or entrada4 or entrada5 or entrada6 or seletor) begin
+    case (seletor)
+      4'b0000 : saida <= entrada1;
+      4'b0001 : saida <= entrada2;
+      4'b0010 : saida <= entrada3;
+      4'b0110 : saida <= entrada4;
+      4'b0111 : saida <= entrada5;
+      4'b1100 : saida <= entrada6;
+    endcase
+end
+
+endmodule
--- ula.v
+++ ula.v
@@ -1,36 +1,44 @@
-`include "mux_4-1.v"
+`include "mux_6-1.v"
 
 // Unidade Logica Aritmetica -> Somador, Subtrador, AND e OR
 module ula (
   input wire entrada1,
   input wire entrada2,
-  input wire [0:1] seletor,
+  input wire [0:3] seletor,
   output wire saida
 );
 
-  reg opSoma;
-  reg opSubtracao;
   reg opAnd;
   reg opOr;
+  reg opSoma;
+  reg opSubtracao;
+  reg opSLT;
+  reg opNor;
 
   // Multiplexador da ULA:
-  // -> 1º = Soma
-  // -> 2º = Subtração
-  // -> 3º = And
-  // -> 4º = Or
-  mux4_1 mux(
-    opSoma,
-    opSubtracao,
+  // -> 1º = AND
+  // -> 2º = OR
+  // -> 3º = ADD
+  // -> 4º = SUB
+  // -> 5º = SLT
+  // -> 6º = NOR
+  mux6_1 mux(
     opAnd,
     opOr,
+    opSoma,
+    opSubtracao,
+    opSLT,
+    opNor,
     seletor,
     saida
   );
 
   initial begin
-    opSoma = entrada1 + entrada2;
-    opSubtracao = entrada1 - entrada2;
     opAnd = entrada1 && entrada2;
     opOr = entrada1 || entrada2;
+    opSoma = entrada1 + entrada2;
+    opSubtracao = entrada1 - entrada2;
+    opSLT = (entrada1 < entrada2);
+    opNor = !(opOr);
   end
 endmodule

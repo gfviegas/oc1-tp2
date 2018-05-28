ad65189de8ab2cb46e7db50dc69593138ba0826f
Gustavo Viegas <webviegas@gmail.com>
2018-05-28 14:29:16 -0300

    Arrumando instrucoes pra non-blocking em instrucoes always

--- aluControl.v
+++ aluControl.v
@@ -12,11 +12,11 @@ module aluControl (
     else
       begin
         case (funct[3:0])
-          4'b0000 : saida = 4'b0010;
-          4'b0010 : saida = 4'b0110;
-          4'b0100 : saida = 4'b0000;
-          4'b0101 : saida = 4'b0001;
-          4'b1010 : saida = 4'b0111;
+          4'b0000 : saida <= 4'b0010;
+          4'b0010 : saida <= 4'b0110;
+          4'b0100 : saida <= 4'b0000;
+          4'b0101 : saida <= 4'b0001;
+          4'b1010 : saida <= 4'b0111;
         endcase
       end
   end

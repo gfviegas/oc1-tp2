41ed71506250f530d75fc985ea6083d04ff1a624
Gustavo Viegas <webviegas@gmail.com>
2018-08-27 21:30:50 -0300

    Alterando a ULA

--- alu.v
+++ alu.v
@@ -1,21 +1,11 @@
-// `include "mux_6-1.v"
-
 // Unidade Logica Aritmetica -> AND, OR, Soma, Subtracao, SLT e NOR
 module alu (
   input wire [31:0] entrada1,
   input wire [31:0] entrada2,
   input wire [0:3] unidadeControle,
-  output wire [0:31] saida,
+  output reg [0:31] saida,
   output reg zero
 );
-
-  reg [31:0] opAnd;
-  reg [31:0] opOr;
-  reg [31:0] opSoma;
-  reg [31:0] opSubtracao;
-  reg [31:0] opSLT;
-  reg [31:0] opNor;
-
   // Multiplexador da ULA:
   // -> 1º = AND
   // -> 2º = OR
@@ -24,25 +14,15 @@ module alu (
   // -> 5º = SLT
   // -> 6º = NOR
   always @(*) begin
-    opAnd <= entrada1 & entrada2;
-    opOr <= entrada1 | entrada2;
-    opSoma <= entrada1 + entrada2;
-    opSubtracao <= entrada1 - entrada2;
-    opSLT <= (entrada1 < entrada2);
-    opNor <= !(opOr);
+    case (unidadeControle)
+      0: saida = entrada1 & entrada2;
+      1: saida = entrada1 | entrada2;
+      2: saida = entrada1 + entrada2;
+      3: saida = entrada1 - entrada2;
+      4: saida = (entrada1 < entrada2);
+      5: saida = !(entrada1 | entrada2);
+    endcase
 
     zero = (saida == 0);
   end
-
-  mux32Bits6 mux(
-    opAnd,
-    opOr,
-    opSoma,
-    opSubtracao,
-    opSLT,
-    opNor,
-    unidadeControle,
-    saida
-  );
-
 endmodule
--- main_TP2.v
+++ main_TP2.v
@@ -31,77 +31,77 @@ module main_TP2(
 	HEX7
 );
 
-//=======================================================
-//  PARAMETER declarations
-//=======================================================
+	//=======================================================
+	//  PARAMETER declarations
+	//=======================================================
 
 
-//=======================================================
-//  PORT declarations
-//=======================================================
+	//=======================================================
+	//  PORT declarations
+	//=======================================================
 
-//////////// CLOCK //////////
-input 		          		CLOCK_50;
-input 		          		CLOCK2_50;
-input 		          		CLOCK3_50;
+	//////////// CLOCK //////////
+	input 		          		CLOCK_50;
+	input 		          		CLOCK2_50;
+	input 		          		CLOCK3_50;
 
-//////////// LED //////////
-output		     [8:0]		LEDG;
-output		    [17:0]		LEDR;
+	//////////// LED //////////
+	output		     [8:0]		LEDG;
+	output		    [17:0]		LEDR;
 
-//////////// KEY //////////
-input 		     [3:0]		KEY;
+	//////////// KEY //////////
+	input 		     [3:0]		KEY;
 
-//////////// SW //////////
-input 		    [17:0]		SW;
+	//////////// SW //////////
+	input 		    [17:0]		SW;
 
-//////////// SEG7 //////////
-output		     [6:0]		HEX0;
-output		     [6:0]		HEX1;
-output		     [6:0]		HEX2;
-output		     [6:0]		HEX3;
-output		     [6:0]		HEX4;
-output		     [6:0]		HEX5;
-output		     [6:0]		HEX6;
-output		     [6:0]		HEX7;
-
-
-//=======================================================
-//  REG/WIRE declarations
-//=======================================================
+	//////////// SEG7 //////////
+	output		     [6:0]		HEX0;
+	output		     [6:0]		HEX1;
+	output		     [6:0]		HEX2;
+	output		     [6:0]		HEX3;
+	output		     [6:0]		HEX4;
+	output		     [6:0]		HEX5;
+	output		     [6:0]		HEX6;
+	output		     [6:0]		HEX7;
 
 
+	//=======================================================
+	//  REG/WIRE declarations
+	//=======================================================
 
 
-//=======================================================
-//  Structural coding
-//=======================================================
 
-wire [31:0] instr;
-reg [31:0] div_clock;
-reg clock;
 
-initial begin
-		div_clock = 0;
-		clock = 0;
-end
+	//=======================================================
+	//  Structural coding
+	//=======================================================
 
-always @ (posedge CLOCK_50) begin
-	div_clock = div_clock + 1;
-	if (div_clock == 50000000) begin
-		div_clock = 0;
-		clock = ~clock;
+	wire [31:0] instr;
+	reg [31:0] div_clock;
+	reg clock;
+
+	initial begin
+			div_clock = 0;
+			clock = 0;
 	end
-end
 
-main m (.clock(clock), .instr(instr), .reset(SW[0]));
-setSeg d1 (instr[31:28], HEX7);
-setSeg d2 (instr[27:24], HEX6);
-setSeg d3 (instr[23:20], HEX5);
-setSeg d4 (instr[19:16], HEX4);
-setSeg d5 (instr[15:12], HEX3);
-setSeg d6 (instr[11:8], HEX2);
-setSeg d7 (instr[7:4], HEX1);
-setSeg d8 (instr[3:0], HEX0);
+	always @ (posedge CLOCK_50) begin
+		div_clock = div_clock + 1;
+		if (div_clock == 50000000) begin
+			div_clock = 0;
+			clock = ~clock;
+		end
+	end
+
+	main m (.clock(clock), .instr(instr), .reset(SW[0]));
+	setSeg d1 (instr[31:28], HEX7);
+	setSeg d2 (instr[27:24], HEX6);
+	setSeg d3 (instr[23:20], HEX5);
+	setSeg d4 (instr[19:16], HEX4);
+	setSeg d5 (instr[15:12], HEX3);
+	setSeg d6 (instr[11:8], HEX2);
+	setSeg d7 (instr[7:4], HEX1);
+	setSeg d8 (instr[3:0], HEX0);
 
 endmodule
